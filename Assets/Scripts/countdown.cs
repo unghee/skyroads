124e7e1ea165dd86a1cdac17078cdc91c69cf23b
JordLee <unghee@umich.edu>
2018-03-16 23:42:11 -0400

    countdown added

---
+++ Assets/Scripts/countdown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class countdown : MonoBehaviour {
+
+	[SerializeField]
+	public float CountdownFrom;
+	[SerializeField]
+	public Text CountdownText;
+
+	void Start() {
+		if (CountdownText == null) {
+			Debug.LogError("STATUS INDICATOR: No text object referenced!");
+		}
+	}
+	void Update()
+	{
+		float time = CountdownFrom - Time.timeSinceLevelLoad;
+		CountdownText.text = "Time left: " + time.ToString("0") + "s";
+
+		if (time <= 0f)
+		{
+			TimeUp();
+		}
+	}
+
+	void TimeUp()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/countdown3d.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdown3d : MonoBehaviour {
+
+	[SerializeField]
+	public float CountdownFrom;
+	[SerializeField]
+	public Text CountdownText;
+//	public GameObject 3D
+
+	void Start() {
+		if (CountdownText == null) {
+			Debug.LogError("STATUS INDICATOR: No text object referenced!");
+			GetComponent<TextMesh>().text = "Ready";
+		}
+	}
+	void Update()
+	{
+		float time = CountdownFrom - Time.timeSinceLevelLoad;
+		GetComponent<TextMesh>().text  = "Time left: " + time.ToString("0") + "s";
+
+		if (time <= 0f)
+		{
+			TimeUp();
+		}
+	}
+
+	void TimeUp()
+	{
+		Destroy(gameObject);
+	}
+}
