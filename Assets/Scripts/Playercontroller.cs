0139848202995eeea22804254b00958a2a542deb
JordLee <unghee@umich.edu>
2018-03-17 14:42:06 -0400

    controller right/left added

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -1,45 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour {
 
-	public float speed;
+	private float speed;
 	private float distToGround;
+	private float i;
 	private Rigidbody rb;
-	private Collider collider;
+	private Collider m_Collider;
+	public GameObject Countdown3d;
+
 
 	void Start()
 	{
 
 		rb = GetComponent<Rigidbody> ();
-		distToGround = collider.bounds.extents.y;
+		m_Collider = GetComponent<Collider>();
+
+		distToGround = m_Collider.bounds.extents.y;
+		speed = 15.0f;
 	}
 
 	bool IsGrounded(){
 //		return Physics.Raycast(transform.position, -Vector3.up,  0.1);
-		return Physics.Raycast(transform.position, -Vector3.up,  distToGround+ 1);
+		return Physics.Raycast(transform.position, -Vector3.up,  distToGround+ 0.1f);
 	}
 
 	void FixedUpdate() 
 	{
-//		float moveHorizontal = Input.GetAxis("Horizontal");
-//		float moveVertical = Input.GetAxis ("Vertical");
-//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 
 		Vector3 fixedmovement = new Vector3 (0, 0.0f, 1);
-		Vector3 jumpmovement = new Vector3 (0, 5, 0);
+		Vector3 jumpmovement = new Vector3 (0, 1, 0);
+		Vector3 Right = new Vector3 (20, 0, 0);
+		Vector3 jumpLeft = new Vector3 (-3, 5, 0);
+
 
 //		rb.AddForce (movement*speed);
 
-		rb.AddForce (fixedmovement * speed);
+		if (Countdown3d.GetComponent<TextMesh> ().text == "START!!") 
+		{
+			rb.AddForce (fixedmovement * speed);
+
+//			while (Input.GetKeyDown (KeyCode.UpArrow)) {
+//				i = i + 1;
+//				speed = speed + i;
+//				rb.AddForce (fixedmovement * speed);
+//			}
+
+		}
 
 		print (IsGrounded ());
+//		print (Countdown3d.GetComponent<TextMesh> ().text);
 
-		if (Input.GetKeyDown (KeyCode.Space) && IsGrounded()) 
+		if (Input.GetKey (KeyCode.Space) && IsGrounded())  
 		{ 
 			rb.AddForce (jumpmovement, ForceMode.Impulse);
+//			rb.AddForce (jumpmovement);
+
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow) && IsGrounded()) 
+		{ 
+			rb.AddForce (transform.right*speed);
+
+		}
+//
+		if (Input.GetKey (KeyCode.LeftArrow) && IsGrounded()) 
+		{ 
+			rb.AddForce (transform.right*speed);
 
 		}
 
--- Assets/Scripts/countdown3d.cs
+++ Assets/Scripts/countdown3d.cs
@@ -6,30 +6,38 @@ using UnityEngine.UI;
 public class countdown3d : MonoBehaviour {
 
 	[SerializeField]
-	public float CountdownFrom;
+//	public float CountdownFrom;
+	private float CountdownFrom;
 	[SerializeField]
-	public Text CountdownText;
+//	public Text CountdownText;
 //	public GameObject 3D
 
 	void Start() {
-		if (CountdownText == null) {
-			Debug.LogError("STATUS INDICATOR: No text object referenced!");
+//		if (CountdownText == null) {
+//			Debug.LogError("STATUS INDICATOR: No text object referenced!");
 			GetComponent<TextMesh>().text = "Ready";
-		}
+		CountdownFrom = 5.0f;
+//		}
 	}
 	void Update()
 	{
-		float time = CountdownFrom - Time.timeSinceLevelLoad;
-		GetComponent<TextMesh>().text  = "Time left: " + time.ToString("0") + "s";
+		
+		float timeDisplay = CountdownFrom - Time.timeSinceLevelLoad;
+//		float time = Time.timeSinceLevelLoad;
+//		GetComponent<TextMesh>().text  = "Time left: " + time.ToString("0") + "s";
 
-		if (time <= 0f)
+		if (timeDisplay <= 3f && timeDisplay > 0f) {
+			GetComponent<TextMesh> ().text = timeDisplay.ToString ("0");
+		}
+		else if (timeDisplay <= 0f)
 		{
-			TimeUp();
+//			TimeUp();
+			GetComponent<TextMesh>().text = "START!!";
 		}
 	}
 
-	void TimeUp()
-	{
-		Destroy(gameObject);
-	}
+//	void TimeUp()
+//	{
+//		Destroy(gameObject);
+//	}
 }
